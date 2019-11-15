5e277917090a9ade15063b32c67ef5bf502bbdf3
Gustavo Máximo <gfmaximo97@gmail.com>
2019-11-15 07:57:38 -0300

    Updates

---
+++ precache-manifest.3d215935f99b254a9bc627e8305e08be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97b8b52535d14a0a5befcf9e415506b",
+    "url": "/React.js-Schwarzmueller-Assignment-2/index.html"
+  },
+  {
+    "revision": "79ad7ce010fb71e38b11",
+    "url": "/React.js-Schwarzmueller-Assignment-2/static/css/main.fc1d96d9.chunk.css"
+  },
+  {
+    "revision": "6fc885ea87806e6bc3e6",
+    "url": "/React.js-Schwarzmueller-Assignment-2/static/js/2.77154754.chunk.js"
+  },
+  {
+    "revision": "79ad7ce010fb71e38b11",
+    "url": "/React.js-Schwarzmueller-Assignment-2/static/js/main.9bb6c252.chunk.js"
+  },
+  {
+    "revision": "566f2beccb05e2294c77",
+    "url": "/React.js-Schwarzmueller-Assignment-2/static/js/runtime-main.a1cfecb3.js"
+  }
+]);
